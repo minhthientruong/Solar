f6365ece30ba410dd78a3f0dae068cdb11612d48
Trương Minh Thiện <114926235+minhthientruong@users.noreply.github.com>
2025-03-19 12:00:31 +0700

    Cập nhật code đọc nhiệt độ & độ ẩm từ EraWidget

--- script.js
+++ script.js
@@ -1,40 +1,29 @@
-// function updateHumidity() {
-//   let humidity = Math.floor(Math.random() * 101); // Giá trị từ 0 đến 100%
-//   document.getElementById("humidityValue").innerText = `${humidity}%`;
+// dữ liệu ngày giờ
+function updateDateTime() {
+  const now = new Date();
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
 
-//   let neonGreen = `rgba(0, 255, 0, 1)`;
-//   let fadeGreen = `rgba(0, 0, 0, 0.3)`; // Màu đen trong suốt nhẹ hơn
+  document.getElementById("time").innerText = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
 
-//   let gaugeHumidity = document.getElementById("gaugeHumidity");
-//   gaugeHumidity.style.background = `conic-gradient(
-//     ${neonGreen} 0%,
-//     ${neonGreen} ${(humidity / 100) * 100}%,
-//     ${fadeGreen} ${(humidity / 100) * 100}%,
-//     ${fadeGreen} 100%
-//   )`;
+  document.getElementById("date").innerText = now.toLocaleDateString(
+    "en-US",
+    options
+  );
+}
 
-//   let glowIntensity = (humidity / 100) * 30;
-//   gaugeHumidity.style.boxShadow = `0 0 ${glowIntensity}px rgba(0, 255, 0, 0.8)`;
-// }
+setInterval(updateDateTime, 1000);
+updateDateTime(); // Gọi ngay lập tức để tránh độ trễ 1 giây ban đầu
 
-// function updateTemperature() {
-//   let temperature = Math.floor(Math.random() * 41); // Giá trị từ 0 đến 40°C
-//   document.getElementById("temperatureValue").innerText = `${temperature}°C`;
-
-//   let neonRed = `rgba(255, 0, 0, 1)`;
-//   let fadeRed = `rgba(255, 0, 0, 0.1)`;
-
-//   let gaugeTemperature = document.getElementById("gaugeTemperature");
-//   gaugeTemperature.style.background = `conic-gradient(
-//     ${neonRed} 0%,
-//     ${neonRed} ${(temperature / 40) * 100}%,
-//     ${fadeRed} ${(temperature / 40) * 100}%,
-//     ${fadeRed} 100%
-//   )`;
-
-//   let glowIntensity = (temperature / 40) * 30;
-//   gaugeTemperature.style.boxShadow = `0 0 ${glowIntensity}px rgba(255, 0, 0, 0.8)`;
-// }
 const eraWidget = new EraWidget();
 eraWidget.init({
   needRealtimeConfigs: true,
